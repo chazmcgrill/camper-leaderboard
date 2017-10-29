57bd2ed57735169e169e8636eea7f5a78147c4e7
chazmcgrill <charlietaylordev@gmail.com>
2017-10-29 19:46:41 +0000

    Add Stylesheet / Fix Webpack Server
    Added Sass and CSS. Fixed issue with script not found in webpack dev server

--- client/index.js
+++ client/index.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends Component {
+  render() {
+    return (
+      <p>working</p>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
