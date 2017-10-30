93d053309d05da1879a062027541273a600e251b
chazmcgrill <charlietaylordev@gmail.com>
2017-10-30 11:56:46 +0000

    API Setup
    Created API request and setup basic render for response

--- client/index.js
+++ client/index.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class TableData extends Component {
+class Camper extends Component {
+
   render() {
     return (
-        <tr>
-          <td>1</td>
-          <td>Tester</td>
-          <td>200</td>
-          <td>4396</td>
-        </tr>
+      <tr>
+        <td>{this.props.rank}</td>
+        <td>{this.props.camper.username}</td>
+        <td>{this.props.camper.recent}</td>
+        <td>{this.props.camper.alltime}</td>
+      </tr>
     )
   }
+  
+}
+
+class CamperList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requestFailed: false
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then((Response) => Response.json())
+      .then((findResponse) => {
+        console.log(findResponse);
+        this.setState({
+          data: findResponse
+        });
+      });
+  }
+
+  render() {
+    if (!this.state.data) {
+      return (
+        <tbody>
+          <tr><td>Loading...</td></tr>
+        </tbody>
+      )
+    }
+    return (
+      <tbody>
+        {this.state.data.map(function(camper, i) {
+          return (
+            <Camper camper={camper} key={i} rank={i + 1} />
+          )
+        })}
+      </tbody>
+    )
+  }
+
 }
 
 class App extends Component {
@@ -27,9 +69,7 @@ class App extends Component {
               <th>All Time</th>
             </tr>
           </thead>
-          <tbody>
-            <TableData />
-          </tbody>
+          <CamperList />
         </table>
       </div>
     )
