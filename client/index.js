6aa6607caafddaf05e5f23d150b240e6b8d328b2
chazmcgrill <charlietaylordev@gmail.com>
2017-10-30 09:29:10 +0000

    Add Basic Components
    Added basic components including table and dummy data

--- client/index.js
+++ client/index.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class TableData extends Component {
+  render() {
+    return (
+        <tr>
+          <td>1</td>
+          <td>Tester</td>
+          <td>200</td>
+          <td>4396</td>
+        </tr>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <p>working</p>
+      <div className="container">
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Camper</th>
+              <th>Last 30 Days</th>
+              <th>All Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TableData />
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
